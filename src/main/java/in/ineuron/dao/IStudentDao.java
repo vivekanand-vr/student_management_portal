db5ca9e4b68d1501c09bac5982486a532a37cee0
vivekanand-vr <bendragon21@gmail.com>
2024-03-20 23:12:36 +0530

    "dao changes"

--- src/main/java/in/ineuron/dao/IStudentDao.java
+++ src/main/java/in/ineuron/dao/IStudentDao.java
@@ -6,11 +6,8 @@ public interface IStudentDao {
 
 	// operations to be implemented
 	public String addStudent(Student student);
-
 	public Student searchStudent(Integer sid);
-
 	public String updateStudent(Student student);
-
 	public String deleteStudent(Integer sid);
 
 }
